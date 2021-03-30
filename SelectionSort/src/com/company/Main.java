1af271acd537cfc51f1adfa0a9f636cc6cf7c0dd
Dhandy Joenathan Kurnia Putra <dhanjoenkp@gmail.com>
2021-03-31 02:52:21 +0700

    Add files via upload

---
+++ SelectionSort/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {-22, 10, 0, 8, 5, -49, -30};
+
+        for (int last = intArray.length - 1; last > 0; last--) {
+            int largest = 0;
+
+            for (int i = 1; i <= last; i++){
+                if(intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, last);
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void swap (int[] array, int i, int j) {
+        if(i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
